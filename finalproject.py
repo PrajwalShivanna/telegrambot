e306dd87b771392caaaca1fba7ea1216c6f35099
PrajwalShivanna <69978304+PrajwalShivanna@users.noreply.github.com>
2020-09-08 20:05:11 +0530

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -5,8 +5,6 @@ x = os.getenv('x') # ADAFRUIT_IO_USERNAME
 y = os.getenv('y') # ADAFRUIT_IO_KEY
 z = os.getenv('z') # TELEGRAM_BOT_TOKEN
 
-
-
 def on(bot,update):
     chat_id=update.message.chat_id
     bot.send_photo(chat_id,photo='https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg')
@@ -14,7 +12,6 @@ def on(bot,update):
     aio = Client(x,y)
     value=Data(value=1)
     value_send=aio.create_data('lightningbot',value)
-
 
 def off(bot,update):
     chat_id=update.message.chat_id
